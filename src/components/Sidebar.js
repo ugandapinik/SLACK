f790e16d5eced122a507a6e0166242d69f3f13f0
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-12 17:04:47 -0330

    Slack: SidebarOption included

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import SidebarOption from './SidebarOption'
+
+
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import CreateIcon from '@material-ui/icons/Create'
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 function Sidebar() {
     return (
@@ -17,6 +29,20 @@ function Sidebar() {
 
                 <CreateIcon />
             </SidebarHeader>
+
+
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+
+            <hr/>
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+
         </SidebarContainer>
     )
 }
