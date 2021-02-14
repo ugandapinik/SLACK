e9208ad5130e33465169062d0a193b6bf7d79ce9
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-14 01:58:00 -0330

    Slack: StyledComponent add the header background color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
 
 
 function Header() {
     return (
-        <div>
-            <h1></h1>
-        </div>
+        <HeaderContainer>
+            {/* Header Left */}
+            <HeaderLeft>
+                <HeaderAvatar />
+                <AccessTimeIcon />
+            </HeaderLeft>
+
+
+
+
+
+            {/* Header middle */}
+
+            {/* Header right */}
+        </HeaderContainer>
     )
 }
 
 export default Header
+
+
+const HeaderContainer = styled.div`
+    display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+`
+
+
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px
+    }
+`
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8
+    }
+`
+
+const HeaderMiddle = styled.div``
+
+const HeaderRight = styled.div``
+
