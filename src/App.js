80b2b0c66e5a52d274d5b1b7ffbb3ae24cf9f207
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-14 00:44:27 -0330

    Slack: React Router addedd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets Build Slack</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/">
+              <h1>THis is a home page</h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
