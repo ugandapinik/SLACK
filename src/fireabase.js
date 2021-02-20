80a9c47d6b85d9a54283865c9b1fe6298d433044
Jewel Mahmud Nimul Shamim <wohhie92@gmail.com>
2021-02-20 00:24:18 -0330

    Firebase: Update firebase credentials

---
+++ src/fireabase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANi2J5VmopeOmxNUUlg1xccGzLF_zcujQ",
+    authDomain: "slack-fac73.firebaseapp.com",
+    projectId: "slack-fac73",
+    storageBucket: "slack-fac73.appspot.com",
+    messagingSenderId: "426089212388",
+    appId: "1:426089212388:web:4aa6e15c7130ef02593ff1"
+};
